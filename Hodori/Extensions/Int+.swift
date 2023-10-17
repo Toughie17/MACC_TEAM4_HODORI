c562070ed19fb245a1781ce42de217d3acab880d
Eric <real.dassy@gmail.com>
2023-10-18 04:33:06 +0900

    feat: Extension Int
    Int 타입의 값을 00:00:00 포맷의 타임 스탬프로 변환해주는 프로퍼티 추가

---
+++ Hodori/Extensions/Int+.swift
@@ -0,0 +1,19 @@
+//
+//  Int+.swift
+//  Hodori
+//
+//  Created by Eric on 10/17/23.
+//
+
+import Foundation
+
+extension Int {
+    var asTimestamp: String {
+        let hour = self / 3600
+        let minute = self / 60 % 60
+        let second = self % 60
+
+        return String(format: "%02i:%02i:%02i", hour, minute, second)
+    }
+}
+
