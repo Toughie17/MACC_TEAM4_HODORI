52e59b7d2fa32fa1b56babf5ea637e8c842fd323
Eric <real.dassy@gmail.com>
2023-10-18 10:24:59 +0900

    refactor: Color Extension
    사용하지 않는 Color들을 제거했습니다.

--- Hodori/Extensions/Color+.swift
+++ Hodori/Extensions/Color+.swift
@@ -10,12 +10,11 @@ import SwiftUI
 extension Color {
     static let pointBlue = Color(UIColor(hexCode: "3C6EEE"))
     static let mainViewBlockGray = Color(UIColor(hexCode: "1C1C1E"))
-    static let pickerGray = Color(UIColor(hexCode: "2C2C30"))
     static let buttonTextDisabled = Color(UIColor(hexCode: "667399"))
     static let buttonDisabled = Color(UIColor(hexCode: "222C48"))
     static let sheetCategoryTextGray = Color(UIColor(hexCode: "6E6F79"))
     static let sheetBlockBackgroundGray = Color(UIColor(hexCode: "262629"))
     static let meetingViewCategoryTextGray = Color(UIColor(hexCode: "797979"))
-    static let pauseButtonRed = Color(UIColor(hexCode: "D63939"))
+    static let progressbarRed = Color(UIColor(hexCode: "D63939"))
     static let timeYellow = Color(UIColor(hexCode: "FFC120"))
 }
