8d4ce22f29d4d7613aa58640b74ed6d640107804
Eric <real.dassy@gmail.com>
2023-10-18 10:17:20 +0900

    feat: TimerManager 시간 관리 기능 추가
    유저가 설정한 시간
    유저가 추가한 모든 시간
    유저가 추가한 시간
    유저가 추가하고 실제로 사용한 시간
    총 소요시간
    프로퍼티를 통해서 전체적인 시간 관리 기능 구현

--- Hodori/Managers/TimerManager.swift
+++ Hodori/Managers/TimerManager.swift
@@ -16,10 +16,6 @@ final class TimerManager: ObservableObject {
     }
     private var timer = Timer()
     
-    var totalTimeForCurrentSelection: Int {
-        (selectedHoursAmount * 3600) + (selectedMinutesAmount * 60) + selectedSecondsAmount
-    }
-
     @Published var selectedHoursAmount = 0
     @Published var selectedMinutesAmount = 0
     @Published var selectedSecondsAmount = 0
@@ -28,13 +24,14 @@ final class TimerManager: ObservableObject {
             switch state {
             case .cancelled:
                 timer.invalidate()
-                secondsToCompletion = 0
+                totalUsedTime = totalTime - secondsToCompletion
                 progress = 0
-
+                
             case .active:
                 startTimer()
-
-                secondsToCompletion = totalTimeForCurrentSelection
+                
+                secondsToCompletion = startTime
+                totalTime = startTime
                 progress = 1.0
 
                 updateCompletionDate()
@@ -48,10 +45,31 @@ final class TimerManager: ObservableObject {
             }
         }
     }
+    
+    // MARK: 유저가 회의 생성 시점에 Picker로 생성한 시간.
+    var startTime: Int {
+        (selectedHoursAmount * 3600) + (selectedMinutesAmount * 60) + selectedSecondsAmount
+    }
 
+    // MARK: 유저가 추가한 모든 시간. 처음엔 startTime 값과 같음. 추후에 startTime + addedTime
+    @Published var totalTime: Int = 0
+    
+    // MARK: 실제로 MeetingView에 보여지는 남은 시간. 처음엔 startTime 값과 같음. 추후에 secondsToCompletion + addedTime
     @Published var secondsToCompletion = 0
-    @Published var progress: Float = 0.0
-    @Published var completionDate = Date.now
+    
+    // MARK: 유저가 시간을 추가했을 때, 이 프로퍼티에 값이 들어감.
+    @Published var addedTime: Int = 0 {
+        didSet {
+            secondsToCompletion += addedTime
+            totalTime += addedTime
+        }
+    }
+    
+    //MARK: 유저가 실제로 사용한 추가 시간.
+    @Published var usedAddedTime: Int = 0
+    
+    // MARK: 총 소요시간. 딱 유저가 쓴 시간. totalTime - secondToCompletion
+    @Published var totalUsedTime: Int = 0
 
     let hoursRange = stride(from: 0, through: 23, by: 1)
     let minutesRange = stride(from: 0, through: 59, by: 5)
@@ -60,10 +78,20 @@ final class TimerManager: ObservableObject {
     private func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [weak self] _ in
             guard let self else { return }
-
-            self.secondsToCompletion -= 1
-            self.progress = Float(self.secondsToCompletion) / Float(self.totalTimeForCurrentSelection)
+            if self.secondsToCompletion <= 0 {
+                self.state = .cancelled
+                return
+            }
             
+            if self.addedTime > 0 {
+                self.usedAddedTime += 1
+            }
+            
+            self.secondsToCompletion -= 1
+            self.progress = Float(self.secondsToCompletion) / Float(self.totalTime)
+        })
+    }
+    
             if self.secondsToCompletion < 0 {
                 self.state = .cancelled
             }
