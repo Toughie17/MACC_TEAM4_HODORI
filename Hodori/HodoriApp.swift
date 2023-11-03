2f062e5fc42ad9d022d0a866d1a60ee0a8f68031
Toughie <airj907@gmail.com>
2023-11-03 16:59:07 +0900

    sprint2 start

--- Hodori/HodoriApp.swift
+++ Hodori/HodoriApp.swift
@@ -5,16 +5,11 @@
 //  Created by Eric on 10/10/23.
 //
 
-import FirebaseCore
 import SwiftUI
 
 @main
 struct HodoriApp: App {
-    
-    init() {
-        FirebaseApp.configure()
-    }
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
