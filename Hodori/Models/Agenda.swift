a68ee137b506d0bd6baeaccbaf4ebfa68ab743d6
Eric <real.dassy@gmail.com>
2023-11-05 01:57:54 +0900

    feat: Agenda 모델 추가
    Agenda 모델을 추가했습니다

---
+++ Hodori/Models/Agenda.swift
@@ -0,0 +1,14 @@
+//
+//  Agenda.swift
+//  Hodori
+//
+//  Created by 송지혁 on 11/5/23.
+//
+
+import Foundation
+
+struct Agenda {
+    var title: String
+    var detail: [String]
+    var isComplete: Bool
+}
