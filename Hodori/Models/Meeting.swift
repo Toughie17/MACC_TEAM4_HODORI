1cb04383a665fa303b4a28c6c4313572e7e7dded
Eric <real.dassy@gmail.com>
2023-11-05 01:56:32 +0900

    feat: Meeting 모델 수정
    프로젝트 방향성에 맞게 Meeting 모델을 수정했습니다

---
+++ Hodori/Models/Meeting.swift
@@ -0,0 +1,14 @@
+//
+//  Meeting.swift
+//  Hodori
+//
+//  Created by 송지혁 on 11/5/23.
+//
+
+import Foundation
+
+struct Meeting {
+    var agendas: [Agenda]
+    var startDate: Date
+    var title: String
+}
