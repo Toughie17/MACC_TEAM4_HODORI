c2e0e59d0e34034b48b96f1d68d5ea20a60de820
Toughie <airj907@gmail.com>
2023-11-07 22:28:05 +0900

    refactor: MeetingAlert 코드 정리

--- Hodori/Views/TestMeetingView/MeetingAlert.swift
+++ Hodori/Views/TestMeetingView/MeetingAlert.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MeetingAlert: View {
     @Binding var showAlert: Bool
+    //MARK: 미팅뷰에서 계산을 하거나, 미팅 매니저에서 카운트 값을 가지고 있으면 좋을듯
     let leftAgenda: Int = 3
     
     var body: some View {
@@ -17,57 +18,81 @@ struct MeetingAlert: View {
                 .fill(.white)
             
             VStack(spacing: 0) {
-                Image(systemName: "exclamationmark.triangle")
-                    .fontWeight(.bold)
-                    .font(.system(size: 32))
-                    .foregroundColor(.blue)
+                
+                triangleImage
                     .padding(.top, 25)
                     .padding(.bottom, 21)
                 
-                Text("아직 안 끝낸 \(leftAgenda)개의 안건이 있어요")
-                    .padding(.bottom, 6)
-                Text("진짜 종료하시겠어요?")
-                    .fontWeight(.bold)
+                textBox
                     .padding(.bottom, 38)
                 
                 Divider()
                 
-                HStack {
-                    Spacer()
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.white)
-                        .overlay (
-                            VStack {
-                                Spacer()
-                                Text("취소")
-                                
-                                Spacer()
-                            }
-                        )
-                        .onTapGesture {
-                            showAlert = false
-                        }
-                    
-                    Divider()
-                    
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.white)
-                        .overlay (
-                            VStack {
-                                Spacer()
-                                Text("종료하기")
-                                    .foregroundColor(.blue)
-                                Spacer()
-                            }
-                        )
-                        .onTapGesture {
-                            showAlert = false
-                        }
-                    Spacer()
-                }
+                buttonBox
             }
         }
         .frame(width: 325, height: 247)
+    }
+}
+
+extension MeetingAlert {
+    
+    private var triangleImage: some View {
+        Image(systemName: "exclamationmark.triangle")
+            .fontWeight(.bold)
+            .font(.system(size: 32))
+            .foregroundColor(.blue)
+    }
+    
+    private var textBox: some View {
+        VStack(spacing: 0) {
+            Text("아직 안 끝낸 \(leftAgenda)개의 안건이 있어요")
+                .padding(.bottom, 6)
+            Text("진짜 종료하시겠어요?")
+                .fontWeight(.bold)
+        }
+    }
+    
+    private var buttonBox: some View {
+        HStack {
+            Spacer()
+            cancelButton
+            Divider()
+            endButton
+            Spacer()
+        }
+    }
+    
+    private var cancelButton: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(.white)
+            .overlay (
+                VStack {
+                    Spacer()
+                    Text("취소")
+                    
+                    Spacer()
+                }
+            )
+            .onTapGesture {
+                showAlert = false
+            }
+    }
+    
+    private var endButton: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(.white)
+            .overlay (
+                VStack {
+                    Spacer()
+                    Text("종료하기")
+                        .foregroundColor(.blue)
+                    Spacer()
+                }
+            )
+            .onTapGesture {
+                showAlert = false
+            }
     }
 }
 
