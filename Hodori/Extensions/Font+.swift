b5ca65be51ffb84e2709b5294491e0c96be66f81
Toughie <airj907@gmail.com>
2023-11-10 14:57:07 +0900

    feat: 커스텀 폰트 추가

---
+++ Hodori/Extensions/Font+.swift
@@ -0,0 +1,14 @@
+//
+//  Font+.swift
+//  Hodori
+//
+//  Created by Toughie on 11/10/23.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // REGULAR
+    static let pretendRegular16: Font = .custom("Pretendard-Regular", size: 30)
+}
