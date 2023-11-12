0f1def088ba901da0466e0b63b5333ec40bc17e2
Eric <real.dassy@gmail.com>
2023-11-13 06:17:42 +0900

    feat: Agenda 모델 Codable Protocol 채택
    Data 타입으로 CoreData 저장하기 위해 encoding/decoding 기능을 제공하는 Codable 프로토콜을 채택

--- Hodori/Models/Agenda.swift
+++ Hodori/Models/Agenda.swift
@@ -7,19 +7,9 @@
 
 import Foundation
 
-struct Agenda: Identifiable, Hashable {
+struct Agenda: Identifiable, Hashable, Codable {
     var title: String
     var detail: [String]
     var isComplete: Bool = false
     var id = UUID().uuidString
 }
-
-// 얘를 계속 넘겨줌
-//@State agendas [Agenda]
-//
-//
-////세부 회의 안건은
-//@State current Title
-//@State current detail
-//
-//agendas.append(
