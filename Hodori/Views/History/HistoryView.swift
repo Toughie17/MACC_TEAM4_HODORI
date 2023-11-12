06142a20e8ee1007be0136f8fd06362765429990
Eric <real.dassy@gmail.com>
2023-11-13 06:22:05 +0900

    test: HistoryView 추가
    추후에 이어 개발할 HistoryView
    StartView에서 Navigation이 되는지 테스트해보기 위해 추가되었습니다

---
+++ Hodori/Views/History/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  Hodori
+//
+//  Created by 송지혁 on 11/13/23.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
