83865852d17d758e018cde5501be7e2939b9d903
Eric <real.dassy@gmail.com>
2023-11-12 20:12:41 +0900

    test: ContentView 네비게이션 버튼 추가
    테스트를 위해 ContentView에서 AgendaSettingView로 가는
    네비게이션 버튼 추가

--- Hodori/ContentView.swift
+++ Hodori/ContentView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @EnvironmentObject var navigationManager: NavigationManager
     
     var body: some View {
-        
         NavigationStack(path: $navigationManager.screenPath) {
-            
             Group {
                 VStack {
                     Image(systemName: "globe")
                         .imageScale(.large)
                         .foregroundStyle(.tint)
                     Text("Hello, world!")
+                    
+                    Button("뷰 이동") {
+                        navigationManager.screenPath.append(.agendaSetting)
+                    }
                 }
             }
             .navigationDestination(for: AppScreen.self) { appScreen in
