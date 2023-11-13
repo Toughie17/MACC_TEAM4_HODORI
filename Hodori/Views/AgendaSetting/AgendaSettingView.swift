3aba1b8bf572e3633700ba2a5db00cc9d0f43848
Eric <real.dassy@gmail.com>
2023-11-13 10:32:06 +0900

    feat: WarningAlert 추가
    네비게이션 백버튼을 눌렀을 때, WarningAlert이 나타납니다

--- Hodori/Views/AgendaSetting/AgendaSettingView.swift
+++ Hodori/Views/AgendaSetting/AgendaSettingView.swift
@@ -11,7 +11,8 @@ struct AgendaSettingView: View {
     @State private var agendas: [Agenda] = []
     @EnvironmentObject var navigationManager: NavigationManager
     @StateObject var keyboardManager = KeyboardManager()
-
+    @State private var showAlert = false
+    
     @State private var buttonHeight: CGFloat = 0
     
     @State private var selectedAgenda: Agenda = .init(title: "", detail: [""])
@@ -23,64 +24,74 @@ struct AgendaSettingView: View {
     var scrollElement: [Agenda: Bool] {
         [selectedAgenda: keyboardManager.isKeyboardDissmissed]
     }
-    
-    
-    
+
     var body: some View {
-        VStack(spacing: 0) {
-            ScrollView(showsIndicators: false) {
-                ScrollViewReader { scrollviewProxy in
-                    agendaField
-                        .onChange(of: selectedAgenda) { newValue in
-                            guard !keyboardManager.isKeyboardDissmissed else { return }
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                withAnimation(.easeOut(duration: 0.5)) {
-                                    scrollviewProxy.scrollTo(selectedAgenda.id, anchor: .bottom)
+        ZStack {
+            VStack(spacing: 0) {
+                ScrollView(showsIndicators: false) {
+                    ScrollViewReader { scrollviewProxy in
+                        agendaField
+                            .onChange(of: selectedAgenda) { newValue in
+                                guard !keyboardManager.isKeyboardDissmissed else { return }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    withAnimation(.easeOut(duration: 0.5)) {
+                                        scrollviewProxy.scrollTo(selectedAgenda.id, anchor: .bottom)
+                                    }
                                 }
                             }
-                        }
-                        .onChange(of: agendas.count) { _ in
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                withAnimation(.easeOut(duration: 0.5)) {
-                                    scrollviewProxy.scrollTo(anchor)
+                            .onChange(of: agendas.count) { _ in
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    withAnimation(.easeOut(duration: 0.5)) {
+                                        scrollviewProxy.scrollTo(anchor)
+                                    }
                                 }
                             }
-                        }
-                        .onChange(of: isAddCellClicked) { _ in
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                withAnimation(.easeOut(duration: 0.5)) {
-                                    scrollviewProxy.scrollTo(anchor)
+                            .onChange(of: isAddCellClicked) { _ in
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    withAnimation(.easeOut(duration: 0.5)) {
+                                        scrollviewProxy.scrollTo(anchor)
+                                    }
                                 }
                             }
-                        }
+                    }
+                } // ScrollView
+                .padding(.top, 27)
+                .background {
+                    touchableArea
                 }
-            } // ScrollView
-            .padding(.top, 27)
-            .background {
-                touchableArea
+                .onTapGesture {
+                    isFocused.toggle()
+                }
+                
+                keyboardArea
+                completeButton
+                    .padding(.bottom, 42)
+            } // VStack
+            .ignoresSafeArea(edges: .bottom)
+            .navigationTitle("새 회의 시작하기")
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        showAlert = true
+                        isFocused.toggle()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .foregroundStyle(.black)
+                    }
+                }
             }
-            .onTapGesture {
-                isFocused.toggle()
-            }
+            .padding(.horizontal, 20)
             
-            keyboardArea
-            completeButton
-                .padding(.bottom, 42)
-        } // VStack
-        .ignoresSafeArea(edges: .bottom)
-        .navigationTitle("새 회의 시작하기")
-        .navigationBarBackButtonHidden()
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button {
-                    navigationManager.screenPath.removeLast()
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .foregroundStyle(.black)
-                }
+            if showAlert {
+                Color.black
+                    .opacity(0.7)
+                    .overlay {
+                        warningAlert
+                    }
+                    .ignoresSafeArea()
             }
         }
-        .padding(.horizontal, 20)
     }
     
     private var touchableArea: some View {
@@ -94,19 +105,10 @@ struct AgendaSettingView: View {
 
     }
     
-    private var keyboardAvoidance: CGFloat {
-        guard keyboardManager.isKeyboardDissmissed else { return keyboardManager.keyboardHeight - buttonHeight - autoCorrectionHeight }
-        return 0
-    }
-    
-    private var keyboardArea: some View {
-        Color.clear
-            .frame(maxHeight: keyboardAvoidance)
-    }
+
     
     private var agendaList: some View {
         ForEach(agendas) { agenda in
-            
             AgendaView(viewState: .normal, agenda: agenda.title, detailAgendas: agenda.detail, isFocused: $isFocused) { agendaTitle, detailAgendas in
                 if agendaTitle.isEmpty, detailAgendas.filter({ $0 != "" && $0 != "\u{200B}" }).isEmpty {
                     updateAgenda(oldAgenda: agenda, newAgenda: Agenda(title: agendaTitle, detail: detailAgendas))
@@ -188,5 +190,105 @@ struct CustomButton: ButtonStyle {
 struct AgendaSettingView_Previews: PreviewProvider {
     static var previews: some View {
         AgendaSettingView()
+    }
+}
+
+// MARK: WarningAlert
+extension AgendaSettingView {
+    private var warningAlert: some View {
+        VStack(spacing: 0) {
+            alertText
+            
+            divider
+            
+            alertButton
+        }
+        .aspectRatio(1.3, contentMode: .fit)
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(.white)
+        }
+        .padding(.horizontal, 34)
+        
+    }
+    
+    private var alertText: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "exclamationmark.triangle")
+                .foregroundStyle(.red)
+                .font(.system(size: 30))
+                .padding(.bottom, 16)
+            
+            Text("페이지를 나가시겠어요?")
+                .font(.system(size: 20))
+                .bold()
+                .padding(.bottom, 8)
+            
+            Text("페이지 이탈 시\n작성했던 내용이 모두 삭제돼요")
+                .font(.system(size: 16))
+                .foregroundStyle(.gray)
+                .opacity(0.8)
+                .multilineTextAlignment(.center)
+        }
+        .padding(.top, 28)
+        .padding(.horizontal, 22)
+        .padding(.bottom, 30)
+        .layoutPriority(1)
+    }
+    
+    private var divider: some View {
+        Rectangle()
+            .fill(.gray)
+            .opacity(0.5)
+            .frame(height: 1)
+    }
+    
+    private var alertButton: some View {
+        HStack {
+            HStack {
+                Spacer()
+                Text("취소")
+                    .font(.system(size: 20))
+                    .fontWeight(.medium)
+                    .foregroundStyle(.gray)
+                Spacer()
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                showAlert = false
+            }
+            
+            Rectangle()
+                .fill(.gray)
+                .opacity(0.5)
+                .frame(width: 1)
+            
+            HStack {
+                Spacer()
+                Text("나가기")
+                    .font(.system(size: 20))
+                    .fontWeight(.medium)
+                    .foregroundStyle(.gray)
+                Spacer()
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                navigationManager.screenPath.removeLast()
+            }
+            
+        }
+    }
+}
+
+// MARK: Keyboard
+extension AgendaSettingView {
+    private var keyboardAvoidance: CGFloat {
+        guard keyboardManager.isKeyboardDissmissed else { return keyboardManager.keyboardHeight - buttonHeight - autoCorrectionHeight }
+        return 0
+    }
+    
+    private var keyboardArea: some View {
+        Color.clear
+            .frame(maxHeight: keyboardAvoidance)
     }
 }
