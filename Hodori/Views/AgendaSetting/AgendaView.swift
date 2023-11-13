d2be9735e2a01ae55763ff6d78896f91fc688199
Eric <real.dassy@gmail.com>
2023-11-13 10:33:59 +0900

    refactor: AgendaView 리팩토링
    각 기능에 맞게 extension으로 분리했습니다.

--- Hodori/Views/AgendaSetting/AgendaView.swift
+++ Hodori/Views/AgendaSetting/AgendaView.swift
@@ -29,8 +29,14 @@ struct AgendaView: View {
         guard let firstDetailAgenda = detailAgendas.first else { return 0 }
         return viewState == .normal && firstDetailAgenda.isEmpty ? 0 : .infinity
     }
+    
     private var cellBackground: some View {
         isCellFocused ? RoundedRectangle(cornerRadius: 16).fill(.gray.opacity(0.2)) : RoundedRectangle(cornerRadius: 16).fill(.clear)
+    }
+    
+    private var isCellEmpty: Bool {
+        guard let detail = detailAgendas.first else { return false }
+        return agenda.isEmpty && detailAgendas.count == 1 && detail.isEmpty
     }
     
     var focus: [Bool: Int?] {
@@ -67,9 +73,91 @@ struct AgendaView: View {
                     cancelFocus()
                 }
         }
-        
+    }
+
+    private var isCellFocused: Bool {
+        agendaFocusField == true || detailAgendaFocusField != nil
     }
     
+    private var isNotFocused: Bool {
+        !isCellFocused
+    }
+    
+    private func resetTextField() {
+        agenda = ""
+        detailAgendas = [""]
+    }
+    
+    private func cancelFocus() {
+        agendaFocusField = false
+        detailAgendaFocusField = nil
+    }
+    
+    private func isFirstWordSpace(text: String) -> Bool {
+        guard let firstWord = text.first, firstWord == " " else { return false }
+        return true
+    }
+    
+    private func addAgenda() {
+        agenda.append("\n")
+    }
+    
+    private func switchState(to state: ViewState) {
+        viewState = state
+    }
+    
+}
+
+
+// MARK: DetailAgenda Logics
+extension AgendaView {
+    private func isNextLineEmpty(current index: Int) -> Bool {
+        index + 1 < detailAgendas.count && detailAgendas[index+1] == "\u{200B}"
+    }
+    
+    private func moveLineTo(index: Int) {
+        detailAgendaFocusField = index
+    }
+    
+    private func isDetailAgendalessThanfive(current index: Int) -> Bool {
+        detailAgendas.count < 5 && index < 4
+    }
+    
+    private func makeNextLine(current index: Int) {
+        detailAgendas.insert("\u{200B}", at: index+1)
+    }
+    
+    private func eraseEmptyDetailAgendas() {
+        detailAgendas = detailAgendas
+            .filter { $0 != "\u{200B}" }
+    }
+    
+    private func detailPlaceholder(current index: Int) -> String {
+        "\(index == 0 && detailAgendas.count == 1 ? "세부 회의 안건" : detailAgendas[index])"
+    }
+    
+    private func handleTapActionInDetailAgenda(current index: Int) {
+        detailAgendas[index].removeAll(where: { $0 == "\n" })
+    }
+}
+
+// MARK: Agenda Logic
+extension AgendaView {
+    private func isUserTapEnter(to text: String) -> Bool {
+        text.contains("\n")
+    }
+    
+    private func isUserTapEnterWithOtherText(to text: String) -> Bool {
+        text != "\n"
+    }
+    
+    private func handleTapActionInAgenda() {
+        agenda.removeAll(where: { $0 == "\n" })
+    }
+}
+
+// MARK: Agenda View
+extension AgendaView {
     private var placeHolder: some View {
         HStack(alignment: .top, spacing: 11) {
             blueCircle
@@ -130,61 +218,6 @@ struct AgendaView: View {
         }
     }
     
-    private var detailAgendaFieldList: some View {
-        ForEach(detailAgendas.indices, id: \.self) { index in
-            HStack(spacing: 8) {
-                Text(bulletPoint)
-                TextField(detailPlaceholder(current: index), text: $detailAgendas[index], axis: .vertical)
-                    .font(.system(size: 14))
-                    .fontWeight(.medium)
-                    .focused($detailAgendaFocusField, equals: index)
-                    .submitLabel(.next)
-                    .disabled(agenda.isEmpty)
-            }
-            .onChange(of: detailAgendas[index]) { newValue in
-                // MARK: 아무것도 추가 안하고 다음 줄로 갈 때
-                guard index < detailAgendas.count else { return }
-                guard isUserTapEnterWithOtherText(to: newValue) else { detailAgendas[index].removeLast()
-                    return }
-                if newValue == "\u{200B}\n" || newValue == "\n" {
-                    detailAgendas[index].removeLast()
-                    return
-                }
-                
-                // MARK: 지우고 이전 줄로 갈 때
-                if newValue.isEmpty {
-                    moveLineTo(index: index - 1)
-                    
-                    // MARK: index가 배열의 존재하는 위치에 있는지 확인 후, 지운 줄 삭제
-                    if index > 0 && index < detailAgendas.count {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                            detailAgendas.remove(at: index)
-                        }
-                    }
-                }
-                
-                // MARK: 다음 줄로 넘길 때
-                guard isUserTapEnter(to: newValue) else { return }
-                handleTapActionInDetailAgenda(current: index)
-
-                if isNextLineEmpty(current: index) {
-                    moveLineTo(index: index+1)
-                } else if isDetailAgendalessThanfive(current: index)  {
-                    makeNextLine(current: index)
-                    moveLineTo(index: index + 1)
-                } else {
-                    addAgenda()
-                }
-            }
-        }
-        .frame(maxHeight: detailAgendasFrame)
-    }
-    
-    private func resetTextField() {
-        agenda = ""
-        detailAgendas = [""]
-    }
-    
     private var agendaField: some View {
         TextField("새 안건", text: $agenda, axis: .vertical)
             .font(.system(size: 16))
@@ -192,6 +225,7 @@ struct AgendaView: View {
             .focused($agendaFocusField)
             .submitLabel(.next)
             .onChange(of: agenda) { newValue in
+                agenda = String(agenda.prefix(21))
                 if isFirstWordSpace(text: newValue) {
                     agenda.removeFirst()
                     return
@@ -228,92 +262,65 @@ struct AgendaView: View {
                 }
             }
     }
-    
-    private var isCellFocused: Bool {
-        agendaFocusField == true || detailAgendaFocusField != nil
-    }
-    
-    private var isNotFocused: Bool {
-        !isCellFocused
-    }
-    
-    private func cancelFocus() {
-        agendaFocusField = false
-        detailAgendaFocusField = nil
-    }
-    
-    private func isFirstWordSpace(text: String) -> Bool {
-        guard let firstWord = text.first, firstWord == " " else { return false }
-        return true
-    }
-    
-    private var isCellEmpty: Bool {
-        guard let detail = detailAgendas.first else { return false }
-        return agenda.isEmpty && detailAgendas.count == 1 && detail.isEmpty
-    }
-    
-    private func addAgenda() {
-        agenda.append("\n")
-    }
-    
-    private func switchState(to state: ViewState) {
-        viewState = state
-    }
-    
 }
 
-
-// MARK: DetailAgenda Logics
+// MARK: DetailAgendas View
 extension AgendaView {
-    private func isNextLineEmpty(current index: Int) -> Bool {
-        index + 1 < detailAgendas.count && detailAgendas[index+1] == "\u{200B}"
-    }
-    
-    private func moveLineTo(index: Int) {
-        detailAgendaFocusField = index
-    }
-    
-    private func isDetailAgendalessThanfive(current index: Int) -> Bool {
-        detailAgendas.count < 5 && index < 4
-    }
-    
-    private func makeNextLine(current index: Int) {
-        detailAgendas.insert("\u{200B}", at: index+1)
-    }
-    
-    private func eraseEmptyDetailAgendas() {
-        detailAgendas = detailAgendas
-            .filter { $0 != "\u{200B}" }
-    }
-    
     private var bulletPoint: String {
         viewState == .normal && detailAgendas.first!.isEmpty ? "" : "•"
     }
     
-    private func detailPlaceholder(current index: Int) -> String {
-        "\(index == 0 && detailAgendas.count == 1 ? "세부 회의 안건" : detailAgendas[index])"
-    }
-    
-    private func handleTapActionInDetailAgenda(current index: Int) {
-        detailAgendas[index].removeAll(where: { $0 == "\n" })
+    private var detailAgendaFieldList: some View {
+        ForEach(detailAgendas.indices, id: \.self) { index in
+            HStack(spacing: 8) {
+                Text(bulletPoint)
+                TextField(detailPlaceholder(current: index), text: $detailAgendas[index], axis: .vertical)
+                    .font(.system(size: 14))
+                    .fontWeight(.medium)
+                    .focused($detailAgendaFocusField, equals: index)
+                    .submitLabel(.next)
+                    .disabled(agenda.isEmpty)
+            }
+            .onChange(of: detailAgendas[index]) { newValue in
+                // MARK: 아무것도 추가 안하고 다음 줄로 갈 때
+                detailAgendas[index] = String(detailAgendas[index].prefix(17))
+                guard index < detailAgendas.count else { return }
+                guard isUserTapEnterWithOtherText(to: newValue) else { detailAgendas[index].removeLast()
+                    return }
+                if newValue == "\u{200B}\n" || newValue == "\n" {
+                    detailAgendas[index].removeLast()
+                    return
+                }
+                
+                // MARK: 지우고 이전 줄로 갈 때
+                if newValue.isEmpty {
+                    moveLineTo(index: index - 1)
+                    
+                    // MARK: index가 배열의 존재하는 위치에 있는지 확인 후, 지운 줄 삭제
+                    if index > 0 && index < detailAgendas.count {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            detailAgendas.remove(at: index)
+                        }
+                    }
+                }
+                
+                // MARK: 다음 줄로 넘길 때
+                guard isUserTapEnter(to: newValue) else { return }
+                handleTapActionInDetailAgenda(current: index)
+
+                if isNextLineEmpty(current: index) {
+                    moveLineTo(index: index+1)
+                } else if isDetailAgendalessThanfive(current: index)  {
+                    makeNextLine(current: index)
+                    moveLineTo(index: index + 1)
+                } else {
+                    addAgenda()
+                }
+            }
+        }
+        .frame(maxHeight: detailAgendasFrame)
     }
 }
-
-// MARK: Agenda Logic
-extension AgendaView {
-    private func isUserTapEnter(to text: String) -> Bool {
-        text.contains("\n")
-    }
-    
-    private func isUserTapEnterWithOtherText(to text: String) -> Bool {
-        text != "\n"
-    }
-    
-    private func handleTapActionInAgenda() {
-        agenda.removeAll(where: { $0 == "\n" })
-    }
-}
-
 
 // MARK: INDEX OUT OF RANGE 원인
 // MARK: 이것만 잡고 자자ㅏㅏ
