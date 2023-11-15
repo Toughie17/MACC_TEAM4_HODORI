8337d75613f14abd3cce3db44d46ac645f33f9f8
Mango <mangodev4@gmail.com>
2023-11-16 08:50:59 +0900

    TimerRunningView 코드 정리.

--- Hodori/Views/TimerView/TimerRunningView.swift
+++ Hodori/Views/TimerView/TimerRunningView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct TimerRunningView: View {
     
     @Binding var sec : Double
+    @Binding var showTimer: Bool
+    
     let date = Date()
-
+    
     @State var timeRemaining : Int = 0
     @State private var blinkTimer: Timer?
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -19,11 +21,7 @@ struct TimerRunningView: View {
     @State private var animate = false
     let feedback = UIImpactFeedbackGenerator(style: .soft)
     @State private var isBlinking = false
-    
-    //    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @Binding var showTimer: Bool
-    
-    
+        
     
     var body: some View {
         VStack {
@@ -43,60 +41,57 @@ struct TimerRunningView: View {
             }
         }
         .ignoresSafeArea(edges: .top)
-//        .background {
-//            Color.gray.opacity(0.5)
-//        }
     }
     
+    
+    func convertSecondsToTime(timeInSeconds: Int) -> String {
+        let hours = timeInSeconds / 3600
+        let minutes = (timeInSeconds - hours*3600) / 60
+        let seconds = timeInSeconds % 60
+        return String(format: "%02i:%02i:%02i", hours,minutes,seconds)
+    }
+    
+    
+    func calcRemain() {
+        let calendar = Calendar.current // 현재 시간 저장
+        let targetTime : Date = calendar.date(byAdding: .second, value: Int($sec.wrappedValue), to: date, wrappingComponents: false) ?? Date() // 설정한 초들을 date 형식으로 바꿔서 calendar에 저장
+        self.timeRemaining =  Int(targetTime.timeIntervalSince(date)) // timeRemaining에 시간 값을 바로 넣게 변형
         
-        func convertSecondsToTime(timeInSeconds: Int) -> String {
-            let hours = timeInSeconds / 3600
-            let minutes = (timeInSeconds - hours*3600) / 60
-            let seconds = timeInSeconds % 60
-            return String(format: "%02i:%02i:%02i", hours,minutes,seconds)
-        }
+    }
+    
+    func stopTimer() {
         
-        
-        func calcRemain() {
-            let calendar = Calendar.current // 현재 시간 저장
-            let targetTime : Date = calendar.date(byAdding: .second, value: Int($sec.wrappedValue), to: date, wrappingComponents: false) ?? Date() // 설정한 초들을 date 형식으로 바꿔서 calendar에 저장
-            self.timeRemaining =  Int(targetTime.timeIntervalSince(date)) // timeRemaining에 시간 값을 바로 넣게 변형
-            
-        }
-        
-        func stopTimer() {
-            
-            self.timer.upstream.connect().cancel()
-        }
-        
-        func startTimer() {
-            self.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-        }
-        private func startStopBlinking() {
-            if isClicked {
-                blinkTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
-                    withAnimation {
-                        self.isBlinking.toggle()
-                    }
+        self.timer.upstream.connect().cancel()
+    }
+    
+    func startTimer() {
+        self.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    }
+    private func startStopBlinking() {
+        if isClicked {
+            blinkTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                withAnimation {
+                    self.isBlinking.toggle()
                 }
-            } else {
-                stopBlinking()
             }
-        }
-        
-        private func stopBlinking() {
-            blinkTimer?.invalidate()
-            blinkTimer = nil
-            withAnimation {
-                self.isBlinking = false
-            }
-        }
-    // 타이머 초기화 함수
-        private func resetTimer() {
-            timeRemaining = 0
-            isClicked = false
+        } else {
             stopBlinking()
         }
+    }
+    
+    private func stopBlinking() {
+        blinkTimer?.invalidate()
+        blinkTimer = nil
+        withAnimation {
+            self.isBlinking = false
+        }
+    }
+    // 타이머 초기화 함수
+    private func resetTimer() {
+        timeRemaining = 0
+        isClicked = false
+        stopBlinking()
+    }
     
     private var playButton: some View {
         Button {
@@ -122,7 +117,7 @@ struct TimerRunningView: View {
         }
         .buttonStyle(PlainButtonStyle())
         .padding(.trailing, 4)
-
+        
     }
     
     private var cancelButton: some View {
@@ -150,7 +145,7 @@ struct TimerRunningView: View {
             .font(.system(size: 40))
             .foregroundColor(isBlinking ? .clear : .black)
             .onAppear {
-                startStopBlinking() // 뷰가 나타날 때 깜빡이기 시작
+                startStopBlinking()
             }
             .onReceive(timer) { _ in
                 if timeRemaining > 0 {
@@ -159,14 +154,11 @@ struct TimerRunningView: View {
                         SoundManager.instance.playSound()
                         stopTimer()
                         feedback.impactOccurred()
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) { //  사운드 fix되면 사운드 길이 확인해서 지속시간 상수로 입력하기.
-                            showTimer = false
-                        } //    10초 지난 후에 showTimer false.
-                        
-                        
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) { //  사운드 fix되면 사운드 길이 확인해서 지속시간 상수로 입력하기.
+//                            showTimer = false
+//                        } //    10초 지난 후에 showTimer false.
                     }
                 }
-                
             }
             .onAppear {
                 calcRemain()
@@ -174,8 +166,7 @@ struct TimerRunningView: View {
             .padding(.vertical, 12)
             .padding(.trailing, 20)
     }
-    
-    }
+}
 
 
 
