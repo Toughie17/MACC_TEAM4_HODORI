7a5a7fa821c758b4b75ed1857f2b4f90fc25ecca
Mango <mangodev4@gmail.com>
2023-11-16 08:37:20 +0900

    TimerSettingView 코드 정리.

--- Hodori/Views/TimerView/TimerSettingView.swift
+++ Hodori/Views/TimerView/TimerSettingView.swift
@@ -17,12 +17,12 @@ struct TimerSettingView: View {
     
     var hourIndex: Int {
         return Int(sec / 3600)
-        }
-
+    }
+    
     var minIndex: Int {
         return Int(sec / 60)
-        }
-   
+    }
+    
     
     var body: some View {
         NavigationView{
@@ -30,65 +30,58 @@ struct TimerSettingView: View {
                 HStack {
                     Spacer()
                     VStack {
-                        Text("타이머 설정하기")
-                            .foregroundColor(.gray2)
-                            .font(.pretendBold20)
-                            .multilineTextAlignment(.center)
-                            .padding(.top, 24)
+                        headLineText
                     }
-                    // 모달 닫기 버튼
-                    Button(action: {
-                        self.presentation.wrappedValue.dismiss()
-                    }) {
-                        Image(systemName:"xmark")
-                            .foregroundColor(.gray3)
-                            .font(.system(size: 24, weight: .regular))
-                            .padding(.top, 23)
-                            .padding(.leading, 89)
-                            .padding(.trailing, 16)
-                    }
-                    
+                    cancelButton
                 }
                 CustomPicker(sec: $sec)
                 
-                
-                //                 타이머 시작 버튼
-                //                Button{TimerRunningView(sec: $sec)}label: {
-                //
-                //
-                //                    ZStack {
-                //                        RoundedRectangle(cornerRadius: 16)
-                //                            .fill(.blue)
-                //                            .frame(width: 353, height: 56)
-                //                        Text("시작")
-                //                            .foregroundColor(.white)
-                ////                            .opacity( ? 1: 0.4)
-                //
-                //                    }
-                //
-                
-                Button{
-                    withAnimation(.bouncy){
-                        showModal.toggle()
-                        showTimer.toggle()
-                    }
-                } label:{
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill((hourIndex == 0 && minIndex == 0 ) ? Color.gray5 : Color.gray1)
-                            .frame(width: 342, height: 54)
-                        Text("타이머 시작")
-                        .foregroundColor(.white)                }
-                    .padding(.horizontal, 24)
-                    .padding(.bottom, 16)
-
-                    
-                }
-                .disabled(hourIndex == 0 && minIndex == 0 )
-                .padding(.horizontal, 20)
+                timerStartButton
                 
             }
         }
+    }
+    
+    private var headLineText: some View {
+        Text("타이머 설정하기")
+            .foregroundColor(.gray2)
+            .font(.pretendBold20)
+            .multilineTextAlignment(.center)
+            .padding(.top, 24)
+    }
+    
+    private var cancelButton: some View {
+        Button(action: {
+            self.presentation.wrappedValue.dismiss()
+        }) {
+            Image(systemName:"xmark")
+                .foregroundColor(.gray3)
+                .font(.system(size: 24, weight: .regular))
+                .padding(.top, 23)
+                .padding(.leading, 89)
+                .padding(.trailing, 16)
+        }
+    }
+    
+    private var timerStartButton: some View {
+        Button {
+            withAnimation(.bouncy){
+                showModal.toggle()
+                showTimer.toggle()
+            }
+        } label:{
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill((hourIndex == 0 && minIndex == 0 ) ? Color.gray5 : Color.gray1)
+                    .frame(width: 342, height: 54)
+                Text("타이머 시작")
+                    .foregroundColor(.white)
+            }
+            .padding(.horizontal, 24)
+            .padding(.bottom, 16)
+        }
+        .disabled(hourIndex == 0 && minIndex == 0 )
+        .padding(.horizontal, 20)
     }
 }
 
