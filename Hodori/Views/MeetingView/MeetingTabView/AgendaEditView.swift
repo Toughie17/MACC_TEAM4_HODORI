104454e014e85bd9d8df3e5000608051f98ab249
Mango <mangodev4@gmail.com>
2024-03-05 05:27:55 +0900

    feat. 진행중인 안건 불러오기 구현
    현재 진행중인 안건 안건수정뷰로 불러오기

--- Hodori/Views/MeetingView/MeetingTabView/AgendaEditView.swift
+++ Hodori/Views/MeetingView/MeetingTabView/AgendaEditView.swift
@@ -8,18 +8,102 @@
 import SwiftUI
 
 struct AgendaEditView: View {
+    @Environment(\.dismiss) var dismiss
     @Environment(\.presentationMode) var presentation
-    @Binding var showModal : Bool
-    let agenda: Agenda
+    @StateObject var keyboardManager = KeyboardManager()
 
+    @Binding var agenda: Agenda
+    @Binding var showEditModal : Bool
+//    @Binding var agendas: [Agenda]
+//    @Binding var selectedTab: Int
+    
+    
+    @State private var isFocused: Bool = true
+    
+//    init(agenda: Binding<Agenda>, showEditModal: Binding<Bool>, agendas: Binding<[Agenda]>, selectedTab: Binding<Int>) {
+//        _agenda = agenda
+//        _showEditModal = showEditModal
+//    }
+    
     var body: some View {
-            Text("EditView")
-//            Text(agenda.title)
-//                .foregroundStyle(agenda.isComplete ? Color.gray5 : Color.gray1)
-//                .font(.pretendMedium16)
-//                .lineLimit(1)
+        VStack (alignment: .leading) {
+            // 타이틀 섹션
+            HStack {
+                Button {
+                    withAnimation(.bouncy) {
+                        self.presentation.wrappedValue.dismiss()
+                    }
+                } label: {
+                    Text("종료")
+                        .font(.system(size: 17, weight: .regular))
+                        .foregroundStyle(Color.gray2)
+                        .padding(.top, 30)
+                        .padding(.leading, 24)
+                        .padding(.bottom, 25)
+                }
+                
+                Spacer()
+                
+                Text("안건 수정하기")
+                    .font(.pretendBold20)
+                    .foregroundStyle(Color.gray2)
+                    .padding(.top, 28)
+                    .padding(.bottom, 23)
+                
+                Spacer()
+                
+                Button {
+                    withAnimation(.bouncy) {
+                        showEditModal.toggle()
+                    }
+                } label: {
+                    Text("완료")
+                        .font(.system(size: 17, weight: .regular))
+                        .foregroundStyle(Color.primaryBlue)
+                        .padding(.top, 30)
+                        .padding(.trailing, 24)
+                        .padding(.bottom, 25)
+                }
+            }
+            
+                Spacer()
+            
+            // 안건, 세부안건 섹션
+            
+            VStack {
+                Text(agenda.title)
+                    .font(.pretendBold24)
+                    .foregroundStyle(agenda.isComplete ? Color.gray3 : Color.black)
+                
+                if agenda.detail != [""] {
+                    ForEach(agenda.detail, id: \.self) { detail in
+                        HStack(spacing: 0) {
+                            Image(systemName: "circle.fill")
+                                .resizable()
+                                .frame(width: 3, height: 3)
+                                .padding(.trailing, 8)
+                            
+                            Text(detail)
+                                .font(.pretendRegular16)
+                                .lineLimit(1)
+                        }
+                        .foregroundStyle(agenda.isComplete ? Color.gray7 : Color.gray2)
+                        .padding(.bottom, 8)
+                    }
+                } else {
+                    EmptyView()
+                }
+                
+                Spacer()
+            }
+//            .onTapGesture {
+//                isFocused.toggle()
+//            }
+
+        }
     }
 }
-#Preview {
-    AgendaEditView(showModal: Binding.constant(false), agenda: Agenda(title: "보리야힘들다", detail: []))
-}
+
+//#Preview {
+//    AgendaEditView(agenda: $agenda, showEditModal: Binding.constant(false))
+//}
--- Hodori/Views/MeetingView/MeetingTabView/MeetingTabViewCell.swift
+++ Hodori/Views/MeetingView/MeetingTabView/MeetingTabViewCell.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 
 struct MeetingTabViewCell: View {
-    let agenda: Agenda
+//    let agenda: Agenda
+    @Binding var agenda: Agenda
     let index: Int
+    
     @Binding var showLottie: Bool
-    @State var showModal: Bool = false
+    @State var showEditModal: Bool = false
+    
     
     let needLeftLine: Bool
     let needRightLine: Bool
@@ -35,142 +38,145 @@ struct MeetingTabViewCell: View {
                         Spacer()
                         Menu {
                             Button(action: {
-                                HapticManager.shared.mediumyHaptic()
+                                //                                HapticManager.shared.mediumyHaptic()
                                 withAnimation(.bouncy) {
-                                    showModal = true
+                                    showEditModal.toggle()
                                 }
                             })  {
                                 Label("수정하기",systemImage: "pencil")
                             }
-                            .sheet(isPresented: $showModal) {
-                                AgendaEditView(showModal: $showModal, agenda: agenda)
-                                    .presentationDetents([.large])
-                                    .presentationDragIndicator(.visible)
-                            }
+                            
                             
                             Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                                 Label("추가하기",systemImage: "pencil")
                             }
                             Button(role : .destructive,action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                                 Label("삭제하기",systemImage: "trash")
-                                    
+                                
                             }
                             
-                                   }label: {
-                                Image(systemName: "ellipsis")
-                            }
-                                   .padding(.trailing,24)
-                                   
+                        }label: {
+                            Image(systemName: "ellipsis")
+                        }
+                        .padding(.trailing,24)
                         
-                                   }
-                                   currentAgendaTitle
-                                .padding(.bottom, 28)
-                                .padding(.trailing, 80)
-                                   agendaDetails
-                                   Spacer()
-                                   }
-                                .padding(.leading, 36)
-                                   }
-                                   //            .padding(.top, 28)
-                                   
-                                   if showLottie {
-                                MeetingLottieView(index: index)
-                            }
-                                   }
-                                   }
-                                   }
-                                   
-                                   extension MeetingTabViewCell {
-                                
-                                private var checkLine: some View {
-                                    HStack(spacing: 0) {
-                                        
-                                        RoundedRectangle(cornerRadius: 30)
-                                            .frame(height: 2)
-                                            .frame(width: 24)
-                                            .foregroundStyle(needLeftLine ? Color.gray9 : Color.clear)
-                                            .padding(.trailing, 12)
-                                        
-                                        Image(systemName: agenda.isComplete ? "checkmark.circle.fill" : "circle")
-                                            .font(.pretendBold21)
-                                            .frame(width: 22, height: 22)
-                                            .foregroundStyle(Color.primaryBlue)
-                                            .padding(.trailing, 12)
-                                        
-                                        RoundedRectangle(cornerRadius: 30)
-                                            .frame(height: 2)
-                                            .frame(maxWidth: .infinity)
-                                            .foregroundStyle(needRightLine ? Color.gray9 : Color.clear)
-                                    }
-                                }
-                                
-                                private var agendaOrderText: some View {
-                                    Text(agendaTitle(forIndex: index))
-                                        .font(.pretendMedium20)
-                                        .foregroundStyle(agenda.isComplete ? Color.gray6 : Color.gray5)
-                                        .foregroundStyle(Color.gray5)
-                                }
-                                
-                                private var currentAgendaTitle: some View {
-                                    Text(agenda.title)
-                                        .font(.pretendBold24)
-                                        .foregroundStyle(agenda.isComplete ? Color.gray3 : Color.black)
-                                }
-                                
-                                private var agendaDetails: some View {
-                                    Group {
-                                        if agenda.detail != [""] {
-                                            ForEach(agenda.detail, id: \.self) { detail in
-                                                HStack(spacing: 0) {
-                                                    Image(systemName: "circle.fill")
-                                                        .resizable()
-                                                        .frame(width: 3, height: 3)
-                                                        .padding(.trailing, 8)
-                                                    
-                                                    Text(detail)
-                                                        .font(.pretendRegular16)
-                                                        .lineLimit(1)
-                                                }
-                                                .foregroundStyle(agenda.isComplete ? Color.gray7 : Color.gray2)
-                                                .padding(.bottom, 8)
-                                            }
-                                        } else {
-                                            EmptyView()
-                                        }
-                                    }
-                                }
-                                
-                                private func agendaTitle(forIndex index: Int) -> String {
-                                    switch index {
-                                    case 0:
-                                        return "첫 번째 안건"
-                                    case 1:
-                                        return "두 번째 안건"
-                                    case 2:
-                                        return "세 번째 안건"
-                                    case 3:
-                                        return "네 번째 안건"
-                                    case 4:
-                                        return "다섯 번째 안건"
-                                    case 5:
-                                        return "여섯 번째 안건"
-                                    case 6:
-                                        return "일곱 번째 안건"
-                                    case 7:
-                                        return "여덟 번째 안건"
-                                    case 8:
-                                        return "아홉 번째 안건"
-                                    case 9:
-                                        return "열 번째 안건"
-                                    default:
-                                        return "알 수 없는 안건"
-                                    }
-                                }
-                            }
-                                   
-                                   #Preview {
-                                ZStack {
-                                    Color.black.ignoresSafeArea()
-                                    MeetingTabViewCell(agenda: Agenda(title: "토끼는 설치류인가 아님 만약 안건이 두줄", detail: ["하이하이하이하이하이하이하이하이하이", "헤이", "호"], isComplete: false), index: 0, showLottie: .constant(false), needLeftLine: true, needRightLine: true)
-                                }
-                            }
+                        
+                    }
+                    currentAgendaTitle
+                        .padding(.bottom, 28)
+                        .padding(.trailing, 80)
+                    agendaDetails
+                    Spacer()
+                }
+                .padding(.leading, 36)
+            }
+            //            .padding(.top, 28)
+            
+            if showLottie {
+                MeetingLottieView(index: index)
+            }
+        }
+        .sheet(isPresented: $showEditModal, content:{
+            AgendaEditView(agenda: $agenda, showEditModal: $showEditModal)
+                .presentationDetents([.height(782)])
+                .presentationDragIndicator(.visible)
+        })
+        
+        
+    }
+}
+
+extension MeetingTabViewCell {
+    
+    private var checkLine: some View {
+        HStack(spacing: 0) {
+            
+            RoundedRectangle(cornerRadius: 30)
+                .frame(height: 2)
+                .frame(width: 24)
+                .foregroundStyle(needLeftLine ? Color.gray9 : Color.clear)
+                .padding(.trailing, 12)
+            
+            Image(systemName: agenda.isComplete ? "checkmark.circle.fill" : "circle")
+                .font(.pretendBold21)
+                .frame(width: 22, height: 22)
+                .foregroundStyle(Color.primaryBlue)
+                .padding(.trailing, 12)
+            
+            RoundedRectangle(cornerRadius: 30)
+                .frame(height: 2)
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(needRightLine ? Color.gray9 : Color.clear)
+        }
+    }
+    
+    private var agendaOrderText: some View {
+        Text(agendaTitle(forIndex: index))
+            .font(.pretendMedium20)
+            .foregroundStyle(agenda.isComplete ? Color.gray6 : Color.gray5)
+            .foregroundStyle(Color.gray5)
+    }
+    
+    private var currentAgendaTitle: some View {
+        Text(agenda.title)
+            .font(.pretendBold24)
+            .foregroundStyle(agenda.isComplete ? Color.gray3 : Color.black)
+    }
+    
+    private var agendaDetails: some View {
+        Group {
+            if agenda.detail != [""] {
+                ForEach(agenda.detail, id: \.self) { detail in
+                    HStack(spacing: 0) {
+                        Image(systemName: "circle.fill")
+                            .resizable()
+                            .frame(width: 3, height: 3)
+                            .padding(.trailing, 8)
+                        
+                        Text(detail)
+                            .font(.pretendRegular16)
+                            .lineLimit(1)
+                    }
+                    .foregroundStyle(agenda.isComplete ? Color.gray7 : Color.gray2)
+                    .padding(.bottom, 8)
+                }
+            } else {
+                EmptyView()
+            }
+        }
+    }
+    
+    private func agendaTitle(forIndex index: Int) -> String {
+        switch index {
+        case 0:
+            return "첫 번째 안건"
+        case 1:
+            return "두 번째 안건"
+        case 2:
+            return "세 번째 안건"
+        case 3:
+            return "네 번째 안건"
+        case 4:
+            return "다섯 번째 안건"
+        case 5:
+            return "여섯 번째 안건"
+        case 6:
+            return "일곱 번째 안건"
+        case 7:
+            return "여덟 번째 안건"
+        case 8:
+            return "아홉 번째 안건"
+        case 9:
+            return "열 번째 안건"
+        default:
+            return "알 수 없는 안건"
+        }
+    }
+}
+
+//#Preview {
+//    ZStack {
+//        Color.black.ignoresSafeArea()
+//        MeetingTabViewCell(agenda: Agenda(title: "토끼는 설치류인가 아님 만약 안건이 두줄", detail: ["하이하이하이하이하이하이하이하이하이", "헤이", "호"], isComplete: false), index: 0, showLottie: .constant(false), needLeftLine: true, needRightLine: true)
+//    }
+//}
