547bfbf49ba9e25655cbbe7cebd2e5f70f58ad61
Mango <mangodev4@gmail.com>
2024-03-05 12:36:39 +0900

    feat. 세부 안건 수정하기 텍스트필드 구현

--- Hodori/Views/MeetingView/MeetingTabView/AgendaEditView.swift
+++ Hodori/Views/MeetingView/MeetingTabView/AgendaEditView.swift
@@ -27,7 +27,7 @@ struct AgendaEditView: View {
     }
     
     var body: some View {
-        VStack (alignment: .leading) {
+        VStack(alignment: .leading) {
             HStack {
                 cancelBtn
                 Spacer()
@@ -35,46 +35,54 @@ struct AgendaEditView: View {
                 Spacer()
                 CompleteBtn
             }
-        }
+            
             Spacer()
             
             // 안건, 세부안건 섹션
-            VStack {
-                VStack {
-                    AgendaTitleSection
-                }
-                .padding(.leading, 24)
-
-                VStack {
-                    if agenda.detail != [""] {
-                        ForEach(agenda.detail, id: \.self) { detail in
-                            HStack(spacing: 0) {
-                                Image(systemName: "circle.fill")
-                                    .resizable()
-                                    .frame(width: 3, height: 3)
-                                    .padding(.trailing, 8)
-                                
-                                Text(detail)
+            VStack(spacing: 0) {
+                AgendaTitleSection
+            }
+            VStack(spacing: 0) {
+                ForEach(agenda.detail.indices, id: \.self) { index in
+                    if index < 5 {
+                        
+                        if agenda.detail[index].isEmpty {
+                            EmptyView()
+                        } else {
+                            HStack {
+                                Text(bulletPoint)
+                                    .padding(.leading, 8)
+                                    .foregroundStyle(Color.gray6)
+                                TextField("", text: $agenda.detail[index], onCommit: {
+                                    if index < agenda.detail.count - 1 {
+                                        let nextIndex = index + 1
+                                        agenda.detail[nextIndex].append("")
+                                    }
+                                })                                    .padding(.vertical, 12)
+                                    .padding(.horizontal, 6)
+                                    .foregroundColor(Color.gray2)
                                     .font(.pretendRegular16)
-                                    .lineLimit(1)
+                                //                                .padding(.bottom, 8)
                             }
-                            .foregroundStyle(agenda.isComplete ? Color.gray7 : Color.gray2)
-                            .padding(.bottom, 8)
+                            
                         }
-                    } else {
-                        EmptyView()
                     }
-                    Spacer()
                 }
+                Spacer()
             }
-        
-            .onAppear {
-                editedTitle = agenda.title
-                editedDetails = agenda.detail
-            }
+            .padding(.leading, 24)
+            //            .padding(.top, 29)
+            .background(isFocused ? .clear : Color.gray9)
+        }
+        .onAppear {
+            editedTitle = agenda.title
+            editedDetails = agenda.detail
         }
     }
+}
 
+
+// MARK: 뷰 익스텐션
 extension AgendaEditView {
     private var cancelBtn: some View {
         Button {
@@ -94,34 +102,45 @@ extension AgendaEditView {
     }
     
     private var headLineText: some View {
-            Text("안건 수정하기")
-                .font(.pretendBold20)
-                .foregroundStyle(Color.gray2)
-                .padding(.top, 28)
-                .padding(.bottom, 23)
-        }
+        Text("안건 수정하기")
+            .font(.pretendBold20)
+            .foregroundStyle(Color.gray2)
+            .padding(.top, 28)
+            .padding(.bottom, 23)
+    }
     
     private var CompleteBtn: some View {
-            Button {
-                HapticManager.shared.mediumyHaptic()
-                withAnimation(.bouncy) {
-                    editedTitle = agenda.title
-                    editedDetails = agenda.detail
-                    self.presentation.wrappedValue.dismiss()
-                }
-            } label: {
-                Text("완료")
-                    .font(.system(size: 17, weight: .regular))
-                    .foregroundStyle(Color.primaryBlue)
-                    .padding(.top, 30)
-                    .padding(.trailing, 24)
-                    .padding(.bottom, 25)
+        Button {
+            HapticManager.shared.mediumyHaptic()
+            withAnimation(.bouncy) {
+                editedTitle = agenda.title
+                editedDetails = agenda.detail
+                self.presentation.wrappedValue.dismiss()
             }
+        } label: {
+            Text("완료")
+                .font(.system(size: 17, weight: .regular))
+                .foregroundStyle(agenda.title.isEmpty ? Color.gray2: Color.primaryBlue)
+                .padding(.top, 30)
+                .padding(.trailing, 24)
+                .padding(.bottom, 25)
         }
+        .disabled(agenda.title.isEmpty)
+    }
     private var AgendaTitleSection: some View {
         TextField("안건을 수정하세요", text: $agenda.title)
             .font(.pretendBold24)
             .foregroundStyle(Color.black)
+            .padding(.vertical, 24)
+            .padding(.leading, 24)
+            .background(isFocused ? Color.gray9 : .clear)
+            .overlay(
+                HStack {
+                    Spacer()
+                    textChecker
+                        .padding(.trailing, 24)
+                }
+            )
             .onAppear{
                 isFocused = true
             }
@@ -130,6 +149,33 @@ extension AgendaEditView {
             }
             .focused($isFocused)
     }
+    
+    private var textChecker: some View {
+        if isFocused {
+            return AnyView(Text("\(agenda.title.count)/16")
+                .font(.pretendRegular14)
+                .foregroundStyle(Color.gray5))
+        } else {
+            return AnyView(EmptyView())
+        }
+    }
+    
+    
+    private func addDetailAgenda() {
+        if agenda.detail.count < 5 {
+            agenda.detail.append("\n")
+        }
+    }
+    
+    private var bulletPoint: String {
+        agenda.detail.first!.isEmpty ? "" : "‧"
+    }
+    
+    private func isFirstWordSpace(text: String) -> Bool {
+        guard let firstWord = text.first, firstWord == " " else { return false }
+        return true
+    }
+    
     
     func resetChanges() {
         agenda.title = editedTitle
